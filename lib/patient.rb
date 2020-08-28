7ee2130a771f7d04249ab60659f2fc81182ac3f1
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-28 02:45:17 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
   
   def appointments
-    Appointment.all.select {|appt| appt.doctor == self}
+    Appointment.all.select {|appt| appt.patient == self}
   end
   
   def patients
